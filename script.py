4cd1676af8f52af562841eebcc61248890a66194
Leandro Borges de Carvalho <borges@MacBook-Pro-de-Leandro.local>
2020-03-26 15:58:12 -0400

    Added other examples

---
+++ script.py
@@ -0,0 +1,13 @@
+first_name = 'Monty'
+last_name = 'Python'
+full_name = first_name + ' ' + last_name
+print(first_name, last_name)
+
+# Describe the sketch comedy group
+name = "Monty Python"
+description = 'sketch comedy group'
+year = 1969
+
+# Introduce them
+sentence = name + ' is a ' + description + ' formed in ' + str(1969)
+print(sentence)
